3b9019b0ea67863af110077e4c046b9be7c25f70
Andrew Niekamp and Joel Waage <andrew+joel@>
2016-06-23 09:27:13 -0700

    add HTML page, JS folder, CSS folder, README with initial specifications

---
+++ README.md
@@ -0,0 +1,3 @@
+## Specifications
+|Step | input | output|
+|-----:|:-----:|:----:|
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//back
+
+
+//front
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+
+    var number = parseInt($("input#number").val());
+    
+  });
+});
