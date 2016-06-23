68cf5be688ed733a0cf2e76a7a3507c3f25bc287
Andrew Niekamp and Joel Waage <andrew+joel@>
2016-06-23 11:59:41 -0700

    adds functionality to convert numbers to array and built function that can be re-used

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,32 @@
 //back
 
 var result = [];
+var parsedNum = [];
 var numerals = ["I", "V", "X", "L", "C", "D", "M"];
 var convert = function(number) {
   result = [];
-  if (number > 0 && number < 4) {
-    for (var i =0; i < number; i++) {
-      result.push(numerals[0]);
-    }
-  } else if (number >= 4 && number < 9) {
-    if (number === 4) {
-      result.push(numerals[0] + numerals[1])
-    } else {
-      result.push(numerals[1]);
-      if (number < 9) {
-        for (var i = 5; i < number; i++) {
-          result.push(numerals[0]);
-        }
+  var translate = function(number, min, max, symbol, secondary) {
+    if (number > min && number < max- 5) {
+      for (var i = min; i < number; i++) {
+        result.push(secondary); //NOT SURE
       }
-    }
-  } else if (number >= 9) {
-    if (number === 9) {
-      result.push(numerals[0] + numerals[2]);
-    } else {
-      result.push(numerals[2]);
-      if (number < 14) {
-        for (var i = 10; i < number; i++) {
-          result.push(numerals[0]);
+    } else if (number >= max - 5 && number < max) {
+      if (number === max - 5) {
+        result.push(secondary + symbol)
+      } else {
+        result.push(symbol);
+        if (number < max) {
+          for (var i = max - 4; i < number; i++) {
+            result.push(secondary);
+          }
         }
       }
     }
   }
+
+  translate(number, 0, 9, 'V', "I");
+
+
 }
 
 //front
@@ -38,9 +34,49 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     event.preventDefault();
 
-    var number = parseInt($("input#number").val());
+    var number = $("input#number").val().split("");
 
+    for (i = 0; i < number.length; i++) {
+      parsedNum.unshift(parseInt(number[i]));
+    }
+    console.log(parsedNum);
+
+    // parsedNum = parseInt(number);
+    //
+    // debugger;
     convert(number);
     console.log(result.join(""));
   });
 });
+
+
+
+
+
+// if (number > 0 && number < 4) {
+//   for (var i =0; i < number; i++) {
+//     result.push(numerals[0]);
+//   }
+// } else if (number >= 4 && number < 9) {
+//   if (number === 4) {
+//     result.push(numerals[0] + numerals[1])
+//   } else {
+//     result.push(numerals[1]);
+//     if (number < 9) {
+//       for (var i = 5; i < number; i++) {
+//         result.push(numerals[0]);
+//       }
+//     }
+//   }
+// } else if (number >= 9) {
+//   if (number === 9) {
+//     result.push(numerals[0] + numerals[2]);
+//   } else {
+//     result.push(numerals[2]);
+//     if (number < 14) {
+//       for (var i = 10; i < number; i++) {
+//         result.push(numerals[0]);
+//       }
+//     }
+//   }
+// }
