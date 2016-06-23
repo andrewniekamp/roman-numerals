2ccd3b11c789d56f6bb8d9ad67c2e4a98794940b
Andrew Niekamp and Joel Waage <andrew+joel@>
2016-06-23 10:24:05 -0700

    add functionality for 1-4

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Specifications
 |Step | input | output|
 |-----:|:-----:|:----:|
+|convert 1 to I| 1| I|
+|convert 2 to II|2|II|
+<!-- |convert 5 to V| 5| V|
+|convert 10 to X| 10| X|
+|convert 50 to L| 50| L|
+|convert 100 to C| 100| C|
+|convert 500 to D| 500| D|
+|convert 1000 to M| 1000| M| -->
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 //back
 
+var result = [];
+var numerals = ["I", "V", "X", "L", "C", "D", "M"];
+var convert = function(number) {
+  result = [];
+  if (number > 0 && number < 4) {
+    for (var i =0; i < number; i++) {
+      result.unshift(numerals[0]);
+    }
+  } else if (number === 4) {
+    result.push(numerals[0] + numerals[1])
+  } else if (number === 5) {
+    result.push(numerals[1]);
+  }
+}
 
 //front
 $(document).ready(function() {
@@ -7,6 +21,8 @@ $(document).ready(function() {
     event.preventDefault();
 
     var number = parseInt($("input#number").val());
-    
+
+    convert(number);
+    console.log(result.join(""));
   });
 });
